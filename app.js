278a3679ebf5767b07392ccb0eedcb8537e2e6dc
Kevin Chandler <im.kevin@me.com>
2016-10-26 17:11:52 -0700

    pug

--- app.js
+++ app.js
@@ -10,8 +10,15 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.set('view engine', 'pug')
+app.set('views', './views');
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index', {
+    title: 'Social Eyes',
+    message: 'Look up social posts by user handle'
+  });
 });
 
 app.listen(PORT, () => {
