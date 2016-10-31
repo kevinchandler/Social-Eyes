d701802506c306f4d6851786a3c7a4dbe4d6fb34
Kevin Chandler <im.kevin@me.com>
2016-10-31 15:10:28 -0700

    better async

--- js/lookup.js
+++ js/lookup.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const request = require('request'),
+      async = require('async'),
       REDDIT_SERVICE = require('./services/reddit-service'),
       TWITTER_SERVICE = require('./services/twitter-service');
 
@@ -13,29 +14,23 @@ module.exports = {
       instagram: null
     }
 
-    // reddit
-    REDDIT_SERVICE.lookup(username, (err, redditPostInfo) => {
-      if ( err ) { return apiResCallback(err) }
-      posts.reddit = redditPostInfo;
-      // apiResCallback(null, posts)
-      // will need to callback after ALL is completed
-    })
-
-    // twitter
-    TWITTER_SERVICE.lookup(username, (err, twitterPostInfo) => {
-      if ( err ) { return apiResCallback(err) }
-      posts.twitter = twitterPostInfo;
-      // apiResCallback(null, posts)
-      // will need to callback after ALL is completed
-    })
-
-    // TODO callback when all services have responded
-    setTimeout(() => {
-      apiResCallback(null, posts)
-    }, 20000);
-
-
-    // instagram
+    async.parallel([
+      (callback) => {
+        TWITTER_SERVICE.lookup(username, (err, twitterPostInfo) => {
+          if ( err ) { return callback(err) }
+          posts.twitter = twitterPostInfo;
+          callback(null, 'twitter')
+        })
+      },
+      (callback) => {
+        REDDIT_SERVICE.lookup(username, (err, redditPostInfo) => {
+          if ( err ) { return callback(err) }
+          posts.reddit = redditPostInfo;
+          callback(null, 'reddit')
+        })
+      }
+    ], (err, results) => {
+         apiResCallback(null, posts)
+    });
   }
-
 }
